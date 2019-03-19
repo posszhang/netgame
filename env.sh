e206bffede0d3ff81a675cec4145728d5bdc116b
posszhang <posszhang@VM_19_222_centos.(none)>
2019-03-19 17:08:18 +0800

    init

---
+++ env.sh
@@ -0,0 +1,15 @@
+go get github.com/golang/protobuf/proto
+go get github.com/gorilla/websocket
+go get github.com/xtaci/kcp-go
+go get github.com/sirupsen/logrus
+
+git clone https://github.com/golang/crypto.git $GOPATH/src/github.com/golang/crypto
+git clone https://github.com/golang/net.git $GOPATH/src/github.com/golang/net
+git clone https://github.com/golang/sys.git $GOPATH/src/github.com/golang/sys
+git clone https://github.com/golang/text.git $GOPATH/src/github.com/golang/text
+git clone https://github.com/golang/lint.git $GOPATH/src/github.com/golang/lint
+git clone https://github.com/golang/tools.git $GOPATH/src/github.com/golang/tools
+
+ln -s $GOPATH/src/github.com/golang/ $GOPATH/src/golang.org/x
+
+
---
+++ proto/msg/.svn/text-base/build.sh.svn-base
@@ -0,0 +1,1 @@
+protoc --go_out=../../src/command ./*.proto
